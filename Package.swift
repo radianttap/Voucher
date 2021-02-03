5ae21d461bfa655f50c30f46a926f043bbe15b0d
Aleksandar Vacić <av@radianttap.com>
2021-02-03 15:57:10 +0100

    Test run

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+	name: "Voucher",
+	platforms: [
+		.iOS(.v12),
+		.tvOS(.v12)
+	],
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "Voucher",
+			targets: ["Voucher"])
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "Voucher",
+			path: "Voucher"
+		),
+	]
+)
